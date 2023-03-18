c5e8a821c8aa36c84f5da75ffa1ef8324332cc5a
Antoine <antoine.thioly@outlook.com>
2023-03-18 10:18:06 +0400

    hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("hello world")
+print("H" + "A" + "P" + "P" + "Y")
+print("hello" * 7)
+print("=" * 10)
+print("I" * 8)
+
+print("*")
+print("**")
+print(-2)
+print(2-6)
+time_in_hours = 5
+time_in_minutes = time_in_hours * 60
+print(time_in_hours, "hours is", time_in_minutes, "minutes" )
